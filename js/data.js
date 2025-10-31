2b1d5b6d7383185dcf07afae4bd665047b66e821
mamama6545 <mamama6545@yahoo.co.jp>
2025-10-31 21:24:09 +0900

    add comma

--- js/data.js
+++ js/data.js
@@ -7,5 +7,5 @@ console.log(45 % 8);
 
 // 文字列の連結
 console.log('45' + '18');
-console.log('今日の天気は' + '晴れ');
-console.log('今日の天気は' + '曇り');
+console.log('今日の天気は、' + '晴れ');
+console.log('今日の天気は、' + '曇り');
